822539c7023173bc2f9f00d0d478565bce3b2179
Evgeny <ru-kio@users.noreply.github.com>
2015-12-16 10:16:25 +0300

    Update build-libopus.sh
    delete lic

--- build-libopus.sh
+++ build-libopus.sh
@@ -2,22 +2,6 @@
 #  Builds libopus for all three current iPhone targets: iPhoneSimulator-i386,
 #  iPhoneOS-armv6, iPhoneOS-armv7.
 #
-#  Copyright 2012 Mike Tigas <mike@tig.as>
-#
-#  Based on work by Felix Schulze on 16.12.10.
-#  Copyright 2010 Felix Schulze. All rights reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#  http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 #
 ###########################################################################
 #  Choose your libopus version and your currently-installed iOS SDK version:
